3850eedaa215e38a23c424dee032ffe07fd670d3
axq <1765828575@qq.com>
2024-10-13 18:37:33 +0800

    10-13

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -7,34 +7,33 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-pub trait SomeTrait {
-    fn some_function(&self) -> bool {
-        true
-    }
-}
+pub trait SomeTrait {  
+    fn some_function(&self) -> bool {  
+        true  
+    }  
+}  
 
-pub trait OtherTrait {
-    fn other_function(&self) -> bool {
-        true
-    }
-}
+pub trait OtherTrait {  
+    fn other_function(&self) -> bool {  
+        true  
+    }  
+}  
 
-struct SomeStruct {}
-struct OtherStruct {}
+struct SomeStruct {}  
+struct OtherStruct {}  
 
-impl SomeTrait for SomeStruct {}
-impl OtherTrait for SomeStruct {}
-impl SomeTrait for OtherStruct {}
-impl OtherTrait for OtherStruct {}
+impl SomeTrait for SomeStruct {}  
+impl OtherTrait for SomeStruct {}  
+impl SomeTrait for OtherStruct {}  
+impl OtherTrait for OtherStruct {}  
 
-// YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
-    item.some_function() && item.other_function()
-}
+// YOU MAY ONLY CHANGE THE NEXT LINE  
+fn some_func(item: impl SomeTrait + OtherTrait)-> bool {  
+    item.some_function() && item.other_function()  
+}  
 
-fn main() {
-    some_func(SomeStruct {});
-    some_func(OtherStruct {});
+fn main() {  
+    some_func(SomeStruct {});  
+    some_func(OtherStruct {});  
 }
